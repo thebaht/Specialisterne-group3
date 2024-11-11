be09171bd36b7b5903e0aa3db2164d548fb19047
thebaht <31901333+thebaht@users.noreply.github.com>
2024-11-11 14:00:32 +0100

    hope this works

--- backend/dbinfo.py
+++
@@ -1,8 +0,0 @@
-
-USERNAME = "root"
-PASSWORD = ""
-HOST = "127.0.0.1"
-PORT = 3306       
-DATABASE = "lagringssystem"
-
-connection_string= f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
