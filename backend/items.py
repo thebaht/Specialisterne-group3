524ea5e34c58458932be4355324c4a71dd4ae55e
spac-25 <cb@specialisterne.com>
2024-11-12 09:40:10 +0100

    Add kw_only=True to dataclasses

--- backend/items.py
+++ backend/items.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(kw_only=True)
 class Item:
     id: int
     manufacturer_id: int
@@ -13,38 +13,38 @@ class Item:
     def get_discounted_price(self):
         return self.price * (1.0 - self.discount)
 
-@dataclass
+@dataclass(kw_only=True)
 class Game(Item):
     player_number: list
     age_range: tuple[int, int]
     genre_id: int
 
-@dataclass
+@dataclass(kw_only=True)
 class BoardGame(Game):
     pass
 
-@dataclass
+@dataclass(kw_only=True)
 class CardGame(Game):
     collectible: bool
 
-@dataclass
+@dataclass(kw_only=True)
 class Figure(Item):
     character_id: int | None
     dimensions: tuple[float, float, float]
 
-@dataclass
+@dataclass(kw_only=True)
 class TabletopFigure(Figure):
     units_in_set: int
     pieces: int
 
-@dataclass
+@dataclass(kw_only=True)
 class CollectibleFigure(Figure):
     pass
 
-@dataclass
+@dataclass(kw_only=True)
 class Tool(Item):
     tool_type_id: int
 
-@dataclass
+@dataclass(kw_only=True)
 class Supply(Item):
     pass
