5b49633fcb251c124b9e449991897cfb036c6d5b
thebaht <31901333+thebaht@users.noreply.github.com>
2024-11-13 13:19:52 +0100

    Co-authored-by: MikkelBruun <MikkelBruun@users.noreply.github.com> Co-authored-by: spac-25 <spac-25@users.noreply.github.com>

--- backend/backend.py
+++ backend/backend.py
@@ -1,22 +1,68 @@
 from dbcontext import *
 # from itemValue import *
-from sqlalchemy import update
+from sqlalchemy import update, and_
+from flask import Flask, jsonify, request
+from factory import *
 
 dbcontext = DatabaseContext()
 dbcontext.clear_database()
-
-session = dbcontext.get_session()
+factory = Factory()
 
 # for game in boardGames:
 #     session.add(game)
 
+app = Flask(__name__)
+
+models = {
+    "manufacturer": Manufacturer,
+    "item": Item,
+    "genre": Genre,
+    "game": Game,
+    "boardgame": BoardGame,
+    "cardgame": CardGame,
+    "character": Character,
+    "figure": Figure,
+    "tabletopfigure": TabletopFigure, 
+    "collectiblefigure": CollectibleFigure,
+    "tooltype": ToolType,
+    "tool": Tool,
+    "supplytype": SupplyType,
+    "supply": Supply,
+}
+
+# Get item(s)
+@app.route('/api/items/<str:table_name>', methods=['GET'])
+def get_items(table_name):
+    session = dbcontext.get_session()
+    table = models.get(table_name.lower())
+    filter = request.get_json()
+    filter = [getattr(table, key) == value for key, value in filter.items()]
+    data = session.query(table).filter(and_(*filter)).all()
+    return jsonify(data), 200
 
 
+# Get item
+@app.route('/api/item/<int:id>', methods=['GET'])
+def get_item(id):
+    session = dbcontext.get_session()
+    data = session.query(Item).filter(Item.id == id).first()
+    return jsonify(data), 200
+
+# create item
+@app.route('/api/item', methods=['POST'])
+def create_item():
+    session = dbcontext.get_session()
+    blueprint = request.get_json().items()
+    blueprint["session"] = session
+    item = factory.createItemFromDict(blueprint)
+    session.add(item)
+    return item.id, 200
 
 
 
 #! Just testing stuff.......................................................................
 if __name__ == '__main__':
+    session = dbcontext.get_session()
     man = Manufacturer(name="games workshop")
     man2 = Manufacturer(name="another workshop")
 
@@ -37,7 +83,7 @@ if __name__ == '__main__':
     session.add(fig)
 
 
-    manq = session.query(Manufacturer).filter(Manufacturer.name == "another workshop").first()
+    manq = session.query(Manufacturer).filter(Manufacturer.name >= "another workshop").first()
     fig2 = TabletopFigure(
         manufacturer=manq,
         name="some other marine",
--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,6 @@
 from typing import List, Optional
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     pass
