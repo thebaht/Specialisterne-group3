32555677ab502e9188de47766c865dfce574fdf4
thebaht <31901333+thebaht@users.noreply.github.com>
2024-11-18 14:59:11 +0100

    asd

--- backend/backend.py
+++ backend/backend.py
@@ -39,11 +39,17 @@ def serialize_model(obj: Base):
 def get_items(table_name):
     session = dbcontext.get_session()
     try:
+        print(f"{"_ "*100}")
+        print(table_name)
+        print(f"{"_ "*100}")
         table = models.TABLES[table_name.lower()]
         try:
             filter = request.json.items()
             filter = [getattr(table, key) == value for key, value in filter]
             data = session.query(table).filter(and_(*filter)).all()
+            print(f"{"_"*100}")
+            print(data)
+            print(f"{"_"*100}")
         except Exception:    
             data = session.query(table).all()
         data = [serialize_model(obj) for obj in data]
@@ -154,42 +160,42 @@ def remove_item(id):
     return "deleted", 200
 
 
-@app.route('/api/test/items/<string:table_name>', methods=['GET'])
-def get_items_test(table_name):
-    nested = dbcontext.start_nested_session()
-    ret = get_items(table_name)
-    dbcontext.rollback_nested_session(nested)
-    return ret
-@app.route('/api/test/item/<string:table_name>/<int:id>', methods=['GET'])
-def get_item_test(table_name, id):
-    nested = dbcontext.start_nested_session()
-    ret = get_item(table_name, id)
-    dbcontext.rollback_nested_session(nested)
-    return ret
-@app.route('/api/test/item/', methods=['POST'])
-def create_item_test():
-    nested = dbcontext.start_nested_session()
-    ret = create_item()
-    dbcontext.rollback_nested_session(nested)
-    return ret
-@app.route('/api/test/item/<string:table_name>/<int:id>', methods=['PUT'])
-def update_item_test(table_name, id):
-    nested = dbcontext.start_nested_session()
-    ret = update_item(table_name, id)
-    dbcontext.rollback_nested_session(nested)
-    return ret
-@app.route('/api/test/items/<string:table_name>', methods=['PUT'])
-def update_items_test(table_name):
-    nested = dbcontext.start_nested_session()
-    ret = update_items(table_name)
-    dbcontext.rollback_nested_session(nested)
-    return ret
-@app.route('/api/test/item/<int:id>', methods=['DELETE'])
-def remove_item_test(id):
-    nested = dbcontext.start_nested_session()
-    ret = remove_item(id)
-    dbcontext.rollback_nested_session(nested)
-    return ret
+# @app.route('/api/test/items/<string:table_name>', methods=['GET'])
+# def get_items_test(table_name):
+#     nested = dbcontext.start_nested_session()
+#     ret = get_items(table_name)
+#     dbcontext.rollback_nested_session(nested)
+#     return ret
+# @app.route('/api/test/item/<string:table_name>/<int:id>', methods=['GET'])
+# def get_item_test(table_name, id):
+#     nested = dbcontext.start_nested_session()
+#     ret = get_item(table_name, id)
+#     dbcontext.rollback_nested_session(nested)
+#     return ret
+# @app.route('/api/test/item/', methods=['POST'])
+# def create_item_test():
+#     nested = dbcontext.start_nested_session()
+#     ret = create_item()
+#     dbcontext.rollback_nested_session(nested)
+#     return ret
+# @app.route('/api/test/item/<string:table_name>/<int:id>', methods=['PUT'])
+# def update_item_test(table_name, id):
+#     nested = dbcontext.start_nested_session()
+#     ret = update_item(table_name, id)
+#     dbcontext.rollback_nested_session(nested)
+#     return ret
+# @app.route('/api/test/items/<string:table_name>', methods=['PUT'])
+# def update_items_test(table_name):
+#     nested = dbcontext.start_nested_session()
+#     ret = update_items(table_name)
+#     dbcontext.rollback_nested_session(nested)
+#     return ret
+# @app.route('/api/test/item/<int:id>', methods=['DELETE'])
+# def remove_item_test(id):
+#     nested = dbcontext.start_nested_session()
+#     ret = remove_item(id)
+#     dbcontext.rollback_nested_session(nested)
+#     return ret
 # if __name__ == "__main__":
 #     populate_db()
 
